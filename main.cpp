a97d085f3ac3698ae671da5144c96cc3ccee05ec
Theo <theo@theo.140.233.243>
2025-11-25 22:37:09 +0700

    add data verification logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <mysqlx/xdevapi.h>
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace mysqlx;
 using namespace std;
 
@@ -10,10 +10,16 @@ RowResult res;
 DocResult docs;
 Row row;
 
-bool isEligible(std::string id){
-    Table tab = db.getTable("akun");
+bool isExist(std::string table, std::string value, std::string where, auto param){
+    Table tab = db.getTable(table);
 
-    return false;
+    res = tab.select(value).where(where + " = :param").bind("param", param).execute();
+    bool result = res.count() > 0;
+    if(!result){
+        return false;
+    }
+    
+    return true;
 }
 
 template <typename T> T data(std::string table, std::string value, std::string where, auto param){
@@ -28,8 +34,8 @@ template <typename T> T data(std::string table, std::string value, std::string w
 
 int login(){
     int option, userId;
-    std::string username, password, email, tanggalLahir;
-    bool isExist;
+    std::string username, password, email, tanggalLahir, temp;
+    bool exist;
 
     Table tab = db.getTable("akun_user");
     
@@ -42,37 +48,46 @@ int login(){
     {
     case 1:
         login:
+        cout << "=== LOGIN ===\n";
         cout << "Email: ";
         cin >> email;
         cout << "Password: ";
         cin >> password;
-        // if password and username does not matched then print invalid input
 
-        res = tab.select("password").where("email = :param").bind("param", email).execute();
-        row = *res.begin();
+        exist = isExist("akun_user", "email", "email", email);
+        if(!exist){
+            cout << "Your email is not registered!\n";
+            goto loginRegister;
+        }
 
-        if(password != row[0].get<std::string>()){
+        temp = data<std::string>("akun_user", "password", "email", email);
+        if(password != temp){
             cout << "Wrong password!\n\n";
             goto login;
         }
 
         break;
     case 2:
+        cout << "=== REGISTER ===\n";
         cout << "Email: ";
         cin >> email;
         cout << "Create username: ";
-        cin >> username;
+        cin.ignore();
+        getline(cin, username);
         cout << "Create password: ";
         cin >> password;
         cout << "Tanggal lahir [YYYY-MM-DD]: ";
         cin >> tanggalLahir;
 
-        res = tab.select("email").where("email = :param").bind("param", email).execute();
-        isExist = res.count() > 0;
-        if(isExist){
+        exist = isExist("akun_user", "email", "email", email);
+        if(exist){
             cout << "Email is already registered!\n";
             goto loginRegister;
         }
+
+        tab.insert("email", "nama_user", "password", "tanggal_lahir")
+            .values(email, username, password, tanggalLahir)
+            .execute();
 
         cout << "Register account successfully!\n";
         break;
@@ -82,183 +97,137 @@ int login(){
         break;
     }
 
-    // RowResult res = tab.select("id_user").where("email = :param").bind("param", email).execute();
-
-    // Row row = *res.begin();
-
     userId = data<int>("akun_user", "id_user", "email", email);
 
     return userId;
 }
 
+// void inputJurusan(){
+//     Table tab = db.getTable();
+//     vector<std::string> ptn;
+//     vector<std::string> jurusan;
+//     bool reInput = true;
+//     int jumlahJurusan;
+//     char confirm;
+
+//     while(reInput){
+//         cout << "Masukkan jumlah jurusan (1-4): ";
+//         cin >> jumlahJurusan;
+//         std::string temp;
+
+//         for(int i = 0; i < jumlahJurusan; i++){
+//             cout << "PTN Pilihan " << i + 1 << ": ";
+//             cin >> temp;
+//             ptn.push_back(temp);
+//                         // sql query
+//             cout << "Program Studi Pilihan " << i + 1 << ": ";
+//             cin >> temp;
+//             jurusan.push_back(temp);
+//         }
+
+//         cout << "Ketik [Y/y] jika sudah benar \n => ";
+//         cin >> confirm;
+
+//         if(confirm == 'Y' || confirm == 'y'){
+//             // insert
+//             reInput = false;
+//         } else {
+//             ptn.clear();
+//             jurusan.clear();
+//         }
+//     }
+// }
+
 int main() {
-    
-    // cara jalanin query mysql
-    // use database
-    // sess.sql("USE snpmb").execute();
     Table tab = db.getTable("akun_user");
-    Collection myColl = db.getCollection("myCollection");
-
-    // QUERY
-    // SqlResult res = sess.sql(
-    //     "SELECT s.student_id, s.name, sc.pk "
-    //     "FROM student s "
-    //     "JOIN snbt_score sc ON s.snbt_id = sc.snbt_id"
-    // ).execute();
-
-    // // PRINT HASIL QUERY prinsipnya kaya array
-    // for (auto row : res) {
-    //     std::cout << row[0] << " " << row[1] << " " << row[2] << "\n";
-    // }
-
-    int option, attempts = 0, jumlahJurusan, userId;
-    std::string username, password, email, ptn, tanggalLahir;
-    std::string jurusan;
-    bool isExist;
-        
-    // login:
-    // cout << "\n=== Login or Register ===\n";
-    // cout << "1. Login\n" << "2. Register\n" << "Enter option: ";
-    // cin >> option;
-    
-    // switch (option)
-    // {
-    // case 1:
-    //     cout << "Email: ";
-    //     cin >> email;
-    //     cout << "Password: ";
-    //     cin >> password;
-    //     username = data<std::string>("akun_user", "nama_user", "email", email);
-    //     userId = data<int>("akun_user", "id_user", "email", email);
-
-    //     // if password and username does not matched then print invalid input
-    //     break;
-    // case 2:
-    //     cout << "Email: ";
-    //     cin >> email;
-    //     cout << "Create username: ";
-    //     cin >> username;
-    //     cout << "Create password: ";
-    //     cin >> password;
-    //     cout << "Tanggal lahir [YYYY-MM-DD]: ";
-    //     cin >> tanggalLahir;
-
-    //     res = tab.select("email").where("email = :param").bind("param", email).execute();
-    //     isExist = res.count() > 0;
-    //     if(isExist){
-    //         cout << "Email is already registered!\n";
-    //         goto login;
-    //     }
-
-    //     cout << "Register account successfully!\n";
-    //     break;
-    // default:
-    //     cout << "Invalid input! Please enter (1-2)\n";
-    //     goto login;
-    //     break;
-    // }
+    int option, userId;
+    std::string username;
 
     userId = login();
-    username = data<std::string>("akun_user", "nama_user", "id_user", userId);
-    bool reInput = true;
+    bool reInput = true, exist;
     char confirm;
-
+    
     do{
-        // res = tab.select("nama").where("id = :param").bind("param", id).execute();
-        cout << "\nWelcome " << username << endl;
-        cout << "Your ID is " << userId << endl;
-        cout << "=== MENU UTAMA ===\n";    
+        cout << "=== MENU UTAMA ===\n";
         cout << "1. Verifikasi Data Siswa dan Sekolah\n";
         cout << "2. Pendaftaran SNBP\n";
         cout << "3. Pendaftaran UTBK-SNBT\n";
         cout << "4. Exit\n";
-        cout << "option: ";
+        cout << "Choose [1-4]: ";
         cin >> option;
         
-        int nisn;
-        std::string nama, tanggal_lahir, sekolah;
+        int nisn, tahunLulus, sekolahId;
+        std::string nama, tempat, tanggalLahir, sekolah, jurusan;
         switch(option){
             case 1:
-            cout << "\nWelcome " << username << endl;
             cout << "=== Verifikasi biodata ===\n";
-            cout << "Nama: ";
-            cin >> nama;
-            cout << "Tanggal Lahir: ";
-            cin >> tanggal_lahir;
-            cout << "Sekolah: ";
-            cin >> sekolah;
-            // cout << "NISN: ";
-            // cout << "NIK: ";
-            // cout << "Jenis kelamin: ";
-            // cout << "Jurusan: ";
-            // cout << "Tahun lulus: ";
-            // cout << "No HP: ";
-            // cout << "Agama: ";
-            // cout << "Alamat: ";
-            // cout << "Penghasilan ayah: ";
-            // cout << "Penghasian ibu: ";
-            // cout << "Jumlah tanggungan: ";
 
-            /*cout << "=== Pendaftaran ===\n";
-            cout << "1. SNBP\n";
-            cout << "2. SNBT\n";
-            cout << "Pilih salah satu: ";
-            cin >> option;*/
-            
-            case 2:
+            exist = isExist("biodata", "id_user", "id_user", userId);
+            if(exist){
+                Table tab = db.getTable("biodata");
+                RowResult res = tab.select("id_biodata", "id_user", "id_sekolah", "nama_lengkap")
+                            .where("id_user = :param")
+                            .bind("param", userId)
+                            .execute();
 
-                // if nisn is not in the isEligible database, print you are not eligible 
-                while(reInput){
-
-                    cout << "=== SNBP ===\n";
-                    cout << "Masukkan jumlah jurusan (1-4): ";
-                    cin >> jumlahJurusan;
-                    
-                for(int i = 0; i < jumlahJurusan; i++){
-                cout << "PTN Pilihan " << i + 1 << ": ";
-                cin >> ptn;
-                // sql query
-                cout << "Program Studi Pilihan " << i + 1 << ": ";
-                cin >> jurusan;
-                // sql query add where (id, jurusan, ptn)
-                // add jurusan and univ in database 
+                cout << "Your data: \n";
+                Row row;
+                
+                username = data<std::string>("biodata", "nama_lengkap", "id_user", userId);
+                cout << "\nWelcome " << username << endl;
+                cout << "Your data has been verified!\n";
+                while((row = res.fetchOne())){
+                    cout << "Id biodata: " << row[0] << endl;
+                    cout << "Id user: " << row[1] << endl;
+                    cout << "Id sekolah: " << row[2] << endl;
+                    cout << "Nama: " << row[3] << endl << endl;
                 }
-                cout << "Ketik [Y/y] jika sudah benar \n => ";
-                cin >> confirm;
+            } else {
+                cout << "NISN: ";
+                cin >> nisn;
+                cout << "Nama: ";
+                cin.ignore();
+                getline(cin, nama);
+                cout << "Tempat lahir: ";
+                cin.ignore();
+                getline(cin, tempat);
+                cout << "Tanggal Lahir [YYYY-MM-DD]: ";
+                cin >> tanggalLahir;
+                cout << "Sekolah: ";
+                cin.ignore();
+                getline(cin, sekolah);
+                cout << "Jurusan: ";
+                cin.ignore();
+                getline(cin, jurusan);
+                cout << "Tahun lulus: ";
+                cin >> tahunLulus;
+                
+                sekolahId = data<int>("sekolah_asal", "id_sekolah", "nama_sekolah", sekolah);
+                tab = db.getTable("biodata");
+                tab.insert("id_user", "id_sekolah", "nisn", "nama_lengkap", "tempat_lahir", "tanggal_lahir", "jurusan", "tahun_lulus")
+                    .values(userId, sekolahId, nisn, nama, tempat, tanggalLahir, jurusan, tahunLulus)
+                    .execute();
 
-                confirm == 'Y' || 'y' ? reInput = false : reInput = true;
-                }
-
-                break;
-        case 3:
-                while(reInput){
-
-                    cout << "=== SNBT ===\n";
-                    cout << "Masukkan jumlah jurusan (1-4): ";
-                    cin >> jumlahJurusan;
-                    
-                for(int i = 0; i < jumlahJurusan; i++){
-                cout << "PTN Pilihan " << i + 1 << ": ";
-                cin >> ptn;
-                // sql query
-                cout << "Program Studi Pilihan " << i + 1 << ": ";
-                cin >> jurusan;
-                // sql query add where (id, jurusan, ptn)
-                // add jurusan and univ in database 
-                }
-                cout << "Ketik [Y/y] jika sudah benar \n => ";
-                cin >> confirm;
-
-                confirm == 'Y' || 'y' ? reInput = false : reInput = true;
-                }
-
-                break;
-        default:
-        cout << "bye";
-            break;
+                cout << "Data updated successfully!\n";
             }
-    }while(!(option > 0 && option < 4));
+            break;
+            case 2:
+                // if nisn is not in the isEligible database, print you are not eligible 
+            cout << "=== SNBP ===\n";
+
+            break;
+            case 3:
+
+                break;
+            case 4:
+                cout << "BYE\n";
+                break;
+            default:
+                cout << "Invalid Input! Masukkan angka [1-4]\n";
+            break;
+        }
+    }while(option != 4);
     
     sess.close();
-
+    return 0;
 }
